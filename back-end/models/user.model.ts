919570afbaddb40c0151ca8ed7418dd79cdd94e1
savinduamalka-infor <savindu.amalka@infor.com>
2026-02-17 14:03:31 +0530

    refactor: update auth configuration and user model types

--- back-end/models/user.model.ts
+++ back-end/models/user.model.ts
@@ -1,8 +1,7 @@
 import mongoose, { Schema } from "mongoose";
-import crypto from "crypto";
 
 export interface IUser {
-  _id: string;
+  _id: Schema.Types.ObjectId;
   email: string;
   name: string;
   role: "Admin" | "Lead" | "Member";
@@ -18,10 +17,6 @@ export interface IUser {
 
 const userSchema = new Schema<IUser>(
   {
-    _id: {
-      type: String,
-      default: () => crypto.randomUUID(),
-    },
     email: {
       type: String,
       required: true,
